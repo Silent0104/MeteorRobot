49c1290eebc12a844a4fd8f04eff246b5c4178c5
Mohit <102339239+Wolf2901@users.noreply.github.com>
2023-03-04 21:22:11 +0530

    Update __main__.py

--- MeteorRobot/__main__.py
+++ MeteorRobot/__main__.py
@@ -108,7 +108,7 @@ buttons = [
     [
         InlineKeyboardButton(text=f"🎵𝑴𝒖𝒔𝒊𝒄🎵", callback_data="source_"),
         InlineKeyboardButton(
-            text=​​"𝑶𝒘𝒏𝒆𝒓", url=https://t.me/Wolf_2904)
+            text="𝑶𝒘𝒏𝒆𝒓", url=https://t.me/Wolf_2904),
     ],
 ]
 
