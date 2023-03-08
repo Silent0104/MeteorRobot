295013759c97c5606881c5f838b0c25dea3a5381
Silent0104 <118383059+Silent0104@users.noreply.github.com>
2023-03-08 20:55:35 +0530

    Update alive.py

--- MeteorRobot/modules/alive.py
+++ MeteorRobot/modules/alive.py
@@ -9,16 +9,16 @@ from MeteorRobot.events import register
 from MeteorRobot import telethn as tbot
 
 
-PHOTO = "https://te.legra.ph/file/d5b568d85d1c1e21d5430.jpg"
+PHOTO = "https://te.legra.ph/file/c8a24a036d02abca16395.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ˹𝗟𝗜𝗚𝗨 𝗕𝗔𝗕𝗬˼🫧.** \n\n"
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm 𝗨𝗡𝗢𝗙𝗙𝗜𝗖𝗜𝗔𝗟 𝗥𝗢𝗕𝗢𝗧.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **Master Mind: [⏤͟͞ 𝗟𝗜𝗚𝗘𝗥 ] × [𓃮]™](https://t.me/Lynx_X_Bot)** \n\n"
+  TEXT += f"⚪ **Master Mind: Roman  × [𓃮]™](https://t.me/lll_Romaan_lll)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/Lynx_X_Bot?start=help"), Button.url("Support", "https://t.me/+PYx22tadVaVhMzY1")]]
+  BUTTON = [[Button.url("Help", "https://t.me/Silent_robo_11?start=help"), Button.url("Support", "https://t.me/+JWHEg2H-FutjOTU1")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
